793ccce6e946ff4e4e248cc3c63521e4bf30c6ad
Ben Moon <software@guiltydolphin.com>
2020-04-12 16:53:12 +0100

    Ensure backslash doesn't escape in Inform

--- inform7.el
+++ inform7.el
@@ -122,6 +122,8 @@
 
 (modify-syntax-entry ?\[ "< n" inform7-mode-syntax-table)
 (modify-syntax-entry ?\] "> n" inform7-mode-syntax-table)
+(modify-syntax-entry ?\" "\"" inform7-mode-syntax-table)
+(modify-syntax-entry ?\\ "." inform7-mode-syntax-table)
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.\\(ni\\|i7\\)\\'" . inform7-mode)) ; Inform 7 source files (aka 'Natural Inform')
