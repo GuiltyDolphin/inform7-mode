a39205a603c8bf9b698ad614ac53c35c37623e01
Ben Moon <software@guiltydolphin.com>
2020-05-02 15:26:32 +0100

    Enable lexical binding

--- inform7.el
+++ inform7.el
@@ -1,4 +1,4 @@
-;;; inform7.el --- Major mode for working with Inform 7 files
+;;; inform7.el --- Major mode for working with Inform 7 files -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2020 Ben Moon
 ;; Author: Ben Moon <software@guiltydolphin.com>
